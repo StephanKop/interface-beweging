fe5ff305d3f0da7a96ec6514fab9c8485254258f
Stephan <stephan.kop@outlook.com>
2020-03-23 18:21:21 +0100

    added real button animations

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ document.getElementById("button1").onclick = function() {
   // document.querySelector("body").style.backgroundColor = "#63b881";
   document.querySelector("body").classList.add("color1");
   document.querySelector("body").classList.remove("color2", "color3");
-  document.getElementById("button1").classList.add("active");
-  document.getElementById("button2").classList.remove("active");
-  document.getElementById("button3").classList.remove("active");
+  // document.getElementById("button1").classList.add("active");
+  // document.getElementById("button2").classList.remove("active");
+  // document.getElementById("button3").classList.remove("active");
 };
 
 document.getElementById("button2").onclick = function() {
@@ -16,9 +16,9 @@ document.getElementById("button2").onclick = function() {
   // document.querySelector("body").style.backgroundColor = "#70b598";
   document.querySelector("body").classList.add("color2");
   document.querySelector("body").classList.remove("color1", "color3");
-  document.getElementById("button2").classList.add("active");
-  document.getElementById("button1").classList.remove("active");
-  document.getElementById("button3").classList.remove("active");
+  // document.getElementById("button2").classList.add("active");
+  // document.getElementById("button1").classList.remove("active");
+  // document.getElementById("button3").classList.remove("active");
 };
 
 document.getElementById("button3").onclick = function() {
@@ -26,7 +26,7 @@ document.getElementById("button3").onclick = function() {
   // document.querySelector("body").style.backgroundColor = "#709fb5";
   document.querySelector("body").classList.add("color3");
   document.querySelector("body").classList.remove("color1", "color2");
-  document.getElementById("button3").classList.add("active");
-  document.getElementById("button1").classList.remove("active");
-  document.getElementById("button2").classList.remove("active");
+  // document.getElementById("button3").classList.add("active");
+  // document.getElementById("button1").classList.remove("active");
+  // document.getElementById("button2").classList.remove("active");
 };
