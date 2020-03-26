6ab35a212188b9e7b185be773af97433cc59ee61
Stephan <stephan.kop@outlook.com>
2020-03-26 11:14:00 +0100

    changed the intro video to mp4

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 document.getElementById("button1").onclick = function() {
-  document.getElementById("video").src = "./img/anim1.webm";
+  document.getElementById("video").src = "./img/anim1.mp4";
   document.getElementById("video").classList.remove("hidden");
   document.getElementById("animateGif").style.display = "none";
   document.getElementById("button1").classList.add("active");
   document.getElementById("button2").classList.remove("active");
   document.getElementById("button3").classList.remove("active");
-  document.getElementById("preview1").style.opacity = "0";
-  document.getElementById("preview2").style.opacity = "100";
-  document.getElementById("preview3").style.opacity = "100";
+  document.getElementById("preview1").classList.add("hidden");
+  document.getElementById("preview2").classList.remove("hidden");
+  document.getElementById("preview3").classList.remove("hidden");
 };
 
 document.getElementById("button2").onclick = function() {
@@ -16,9 +16,9 @@ document.getElementById("button2").onclick = function() {
   document.getElementById("button2").classList.add("active");
   document.getElementById("button1").classList.remove("active");
   document.getElementById("button3").classList.remove("active");
-  document.getElementById("preview1").style.opacity = "100";
-  document.getElementById("preview2").style.opacity = "0";
-  document.getElementById("preview3").style.opacity = "100";
+  document.getElementById("preview1").classList.remove("hidden");
+  document.getElementById("preview2").classList.add("hidden");
+  document.getElementById("preview3").classList.remove("hidden");
 };
 
 document.getElementById("button3").onclick = function() {
@@ -27,7 +27,7 @@ document.getElementById("button3").onclick = function() {
   document.getElementById("button3").classList.add("active");
   document.getElementById("button1").classList.remove("active");
   document.getElementById("button2").classList.remove("active");
-  document.getElementById("preview1").style.opacity = "100";
-  document.getElementById("preview2").style.opacity = "100";
-  document.getElementById("preview3").style.opacity = "0";
+  document.getElementById("preview1").classList.remove("hidden");
+  document.getElementById("preview2").classList.remove("hidden");
+  document.getElementById("preview3").classList.add("hidden");
 };
